0728efcc359e8ecfa6e9e99d5ea2f09e78033ad7
s43491 <s43491@bj.ac.th>
2025-11-12 11:05:04 +0700

    Add files via upload

---
+++ a1_platform_29/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1762920101,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/coin2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
